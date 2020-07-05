c34286c40585deb3b7858b300dab9303ac57f77a
pustylnikov <yura.pustylnikov@gmail.com>
2020-07-05 23:55:58 +0300

    installs module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@anvilapp/react-native-loading-view': './src',
+        },
+        cwd: 'babelrc',
+      },
+    ],
+  ],
 }
